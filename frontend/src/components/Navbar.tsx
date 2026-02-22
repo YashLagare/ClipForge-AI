35ad6639d2063ac1e070f3cbe7553142d22836bc
YashLagare <yashlagare77@gmail.com>
2026-02-22 22:37:21 +0530

    updated-code-22-2-2026

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -87,10 +87,10 @@ export default function Navbar() {
                     </a>
                 ))}
 
-                <button onClick={() => setIsOpen(false)} className='font-medium text-gray-300 hover:text-white transition'>
+                <button onClick={() => { setIsOpen(false); openSignIn() }} className='font-medium text-gray-300 hover:text-white transition'>
                     Sign in
                 </button>
-                <PrimaryButton onClick={() => setIsOpen(false)}>Get Started</PrimaryButton>
+                <PrimaryButton onClick={() => {setIsOpen(false); openSignUp()}}>Get Started</PrimaryButton>
 
                 <button
                     onClick={() => setIsOpen(false)}
